7d94d7224faf6c95f766ee3f459faa7d233bf9f8
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-10-14 17:59:19 +0900

    Made Navbar links clickable

--- js/router.js
+++ js/router.js
@@ -2,8 +2,12 @@
 // Step 1: Create the Router
 const route = (event) => { // We define a variable called "route" which we pass an event argument into.
     event = event || window.event; // Captures the click event for the link.
-    event.preventDefault(); // We prevent the natural behavior of the link when it's clicked.
-    window.history.pushState({}, "", event.target.href); // This updates the URL in the browser without reloading the page.
+    const target = event.target.closest("[data-link]");
+    if (!target) return;
+    const url = target.getAttribute("href");
+    if (url.startsWith("http")) return;
+    event.preventDefault();  // We prevent the natural behavior of the link when it's clicked.
+    window.history.pushState({}, "", target.href); // This updates the URL in the browser without reloading the page.
     handleLocation();
 };
 
@@ -15,7 +19,6 @@ const route = (event) => { // We define a variable called "route" which we pass 
         "/experience": "/pages/experience.html",
         "/projects": "/pages/projects.html",
     };
-
 
 
 //Step 2: Create a function that will handlie the changing of our location whenever navigation occurs. 
